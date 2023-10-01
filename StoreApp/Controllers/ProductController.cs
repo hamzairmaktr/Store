2ab17b18035ae5e05950806f28c5a47e27d301e4
Hamza Irmak <hamzairmak46tr@gmail.com>
2023-10-01 11:34:12 +0300

    first commit

---
+++ StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _repositoryContext;
+
+        public ProductController(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _repositoryContext.Products.ToList();
+            return View(model);
+        }
+        public IActionResult Get(int id)
+        {
+            var res = _repositoryContext.Products.SingleOrDefault(p => p.Id.Equals(id));
+            return View(res);
+        }
+    }
+}
---
+++ StoreApp/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public String? ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
